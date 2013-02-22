7de753790de5f5f26a1f4b5bb4ac7e1834933997
fingul <fingul@gmail.com>
2013-02-22 21:55:17 +0900

    add font

--- s2i.py
+++ s2i.py
@@ -1,16 +1,20 @@
 # coding=utf-8
 import os
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
-def s2i(size,msg):
+
+def s2i(size,msg, fontsize=10):
     W, H = size#(100,100)
     #msg = "100"
 
     #http://nadiana.com/pil-tutorial-basic-advanced-drawing#Drawing_a_Rectangle
     im = Image.new("RGBA",(W,H))
     draw = ImageDraw.Draw(im)
-    w, h = draw.textsize(msg)
-    draw.text(((W-w)/2,(H-h)/2), msg, fill="black")
+
+
+    font = ImageFont.truetype("font/NanumGothic.ttf", fontsize)
+    w, h = draw.textsize(msg,font=font)
+    draw.text(((W-w)/2,(H-h)/2), msg, fill="black",font=font)
     border = 1
     draw_rect = (0, 0, W-border,H-border)
     draw.ellipse(draw_rect,outline="black")
@@ -18,14 +22,14 @@ def s2i(size,msg):
     return im
 
 def main():
-    p = 'c:/0.png'
+    p = '0.png'
     size = (100,100)
-    #this not working...
-    #msg = u'사랑해'.encode('utf8')
-    msg = "7942"
+
+    msg = u'사랑해'#.encode('utf8')
+    #msg = "7942"
     i = s2i(size,msg)
     i.save(p, "PNG")
-    os.startfile(p)
+    #os.startfile(p)
 
 if __name__ == "__main__":
     main()
